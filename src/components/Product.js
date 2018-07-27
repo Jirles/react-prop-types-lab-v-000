eba60b5f31d689ea3789e44d61cb29363e446cfa
Emily Jirles <emily.jirles@gmail.com>
2018-07-27 14:00:56 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,7 +9,7 @@ export default class Product extends React.Component {
         <h3>{this.props.name}</h3>
         <small>{this.props.producer}</small>
         <ul>
-          
+          <li></li>
         </ul>
       </div>
     )
